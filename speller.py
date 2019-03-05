86b19175fafe25032458862f8211220f20cb1d9a
Allan Porter <aport550@gmail.com>
2019-03-05 01:17:21 -0500

    Added another test file, changed app functionality, fixed bugs.

--- speller.py
+++ speller.py
@@ -5,66 +5,68 @@ user_file = sys.argv[-1]
 
 def speller(file):
     
-    # create a list for the wordlist
+    # create an empty list later to be used as the dictionary
     dict = []
     
-    #add all the words from our dictionary into the dictionary list with their correct formats
+    #fill our dict list with all the words from our dictionary file, all lowercased and endlines removed for formatting
     with open('wordlist.txt') as f:
         for word in f:
             word = word[:-1]
             word = word.lower()
             dict.append(word)
+            
+    #add these to dict so they dont come up as mispelled
+    endlines = ["\n", "\t", '', "\n\n" , "\n\n\n"]
+    symbols = [".", "...", "?", "!", "-", "_", "+", "=", "(", ")", "[", "]", ";", ":"]
 
-    #create a list for the input words
+    for line in endlines:
+        dict.append(line)
+    for symbol in symbols:
+        dict.append(symbol)
+    
+    #create a list for the words in the file user uploaded
     words_list = []
 
-    #place all the input words into a list so they can be compared with the dictionary list
+    #place all the user file words into a list so they can be later compared with the dictionary list
     with open(file) as g:
-        g = g.read()
+        g =  g.read()
         line = g.split(" ")
 
-    #check for capitals
-    capital_words = []
+    #check for capitals so we can check if base word is misspelled
     for l in line:
-        first_char = (l[0])
-        if first_char.isupper():
-            l = l.lower()
-            capital_words.append(l)
+        if l.istitle():
+            lower_case = l.lower()
+            if lower_case in dict:
+                dict.append(l)
+            else:
+                pass
         words_list.append(l)
 
-    #check for periods
-    count = 0
-    lwos = []
-
+    #check if words contain symbols or endlines if so check if their base word is spelled correctly
     for w in words_list:
-        if w[len(w)-1] == ".":
-            this_word = words_list[count]
-            words_list[count] = this_word[:-1]
-            lwos.append(words_list[count])
-            count = count+1
+        char_count = 0
+        base_word = "zxzxxxxx"
+        for char in w:
+            if char in symbols or char in endlines:
+                base_word = w[:-char_count]
+                char_count = char_count + 1
+            else:
+                char_count = char_count + 1
+        if base_word in dict:
+            dict.append(w)
         else:
-            count = count+1
+            pass
 
-#compare word list to dict list and see if there are words from words list that aren't in dict list if so mark as mispelled
-
-    def neither_in(a, b, c):
-        d = b+c
-        if(a not in d):
-            return True
-        else:
-            return False
-
+    # Compare our user word list to the dictionary word list and see which words match and are spelled correctly
+    print("\nYour file was checked succesfully! Below is the content of your file with mispelled words indicated by a (Mispelled) added onto them.\n")
+          
     for n in words_list:
         if n in dict:
-            if neither_in(n, lwos, capital_words):
-                print(n),
-            if(n in capital_words):
-                n = n.capitalize()
-                print(n),
-            if(n in lwos):
-                print(n+"."),
+             print(n + " ", end='')
         else:
-            print(n+"(Mispelled)"),
+            print(n+"(Mispelled) ", end=''), #mispelled words are denoted as having (Mispelled) added after them
+
+    print('\n')
     return words_list;
 
 speller(user_file)
